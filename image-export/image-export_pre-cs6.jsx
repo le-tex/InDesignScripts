62f260734d06469f7ac4795fc2700f643ca4d974
mkraetke <mkraetke@gmx.de>
2015-12-21 13:42:51 +0100

    update feature set for InDesign 5.0 script

--- image-export/image-export_pre-cs6.jsx
+++ image-export/image-export_pre-cs6.jsx
@@ -1,71 +1,79 @@
 ﻿/*
  * image-export.jsx
- * 
- * 
+ *
+ *
  * Export images from an InDesign document to web-friendly formats.
  *
- * 
- * Note: this script requires at least InDesign Version 8.0 (CS6).
- * 
- * 
+ *
+ * Note: this script works with InDesign version 5.0 and above. Previous
+ *       versions may compatible, too but not tested yet.
+ *
+ *
  * Authors: Gregor Fellenz (twitter: @grefel), Martin Kraetke (@mkraetke)
  *
- * 
+ *
  * LICENSE
- * 
+ *
  * Copyright (c) 2015, Gregor Fellenz and le-tex publishing services GmbH
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
- * 1. Redistributions of source code must retain the above copyright notice, 
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
  * this list of conditions and the following disclaimer.
- * 
- * 2. Redistributions in binary form must reproduce the above copyright notice, 
- * this list of conditions and the following disclaimer in the documentation 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
  * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  */
 
 /*
  * set image export preferences
  */
+
+lang = {
+  pre: 1 // en = 0, de = 1
+}
+
 image = {
     minExportDPI:1,
     maxExportDPI:2400,
     exportDPI:144,
+    maxResolution:4000000,
     exportDir:"export",
     exportQuality:2,
-    exportFormat:"JPG",
     pageItemLabel:"letex:fileName"
 }
 /*
- * set panel preferences 
+ * set panel preferences
  */
 panel = {
-    title:"export images",
-    exportPathTitle:"Store images to",
-    densityTitle:"Density (ppi)",
-    qualityTitle:"Quality",
-    qualityValues:["max", "high", "medium", "low"],
-    selectDirButtonTitle:"Choose",
-    selectDirMenuTitle:"Choose a directory",
-    progressBarTitle:"export Images",
-    noValidLinks:"No valid links found.",
+    title:["Export Images", "Bilder exportieren"][lang.pre],
+    densityTitle:["Density (ppi)", "Auflösung (ppi)"][lang.pre],
+    qualityTitle:["Quality", "Qualität"][lang.pre],
+    qualityValues:[["max", "high", "medium", "low"], ["Maximum", "Hoch", "Mittel", "Niedrig"]][lang.pre],
+    optionsTitle:["Options", "Optionen"][lang.pre],
+    maxResolutionTitle:["Max Resolution (px)", "Maximale Auflösung (px)"][lang.pre],
+    selectDirButtonTitle:["Choose", "Auswählen"][lang.pre],
+    selectDirMenuTitle:["Choose a directory", "Verzeichnis auswählen"][lang.pre],
+    progressBarTitle:["export Images", "Bilder exportieren"][lang.pre],
+    noValidLinks:["No valid links found.", "Keine Bild-Verknüpfungen gefunden"][lang.pre],
+    finishedMessage:["images exported.", "Bilder exportiert."][lang.pre],
     buttonOK:"OK",
-    buttonCancel:"Cancel"
+    buttonCancel:["Cancel", "Abbrechen"][lang.pre]
 }
 
 /*
@@ -104,15 +112,46 @@ function main(){
     }
     // show window
     try {
+        jsExtensions();
         exportImages(doc);
     } catch (e) {
         alert ("Error:\n" + e);
     }
 }
 
+function jsExtensions(){
+  // indexOf
+  if (!Array.prototype.indexOf)
+  {
+     Array.prototype.indexOf = function(elt /*, from*/)
+     {
+        var len = this.length;
+
+        var from = Number(arguments[1]) || 0;
+        from = (from < 0)
+        ? Math.ceil(from)
+        : Math.floor(from);
+
+        if (from < 0)
+        from += len;
+
+        for (; from < len; from++)
+        {
+           if (from in this &&
+           this[from] === elt)
+           return from;
+        }
+        return -1;
+     };
+  }
+}
+
 function exportImages(doc){
     if(drawWindow()){
-        getFilelinks(doc);
+      // overwrite internal Measurement Units to pixels
+      app.scriptPreferences.measurementUnit = MeasurementUnits.PIXELS;
+      // grab file links and export
+      getFilelinks(doc);
     } else {
         return;
     }
@@ -122,8 +161,8 @@ function drawWindow(){
     var myWindow = new Window("dialog", panel.title, undefined, {resizable:true});
         with (myWindow) {
             myWindow.orientation = "column";
-            myWindow.alignChildren ="left";
-            myWindow.formPath = add("panel", undefined, panel.exportPathTitle);
+            myWindow.alignChildren ="fill";
+            myWindow.formPath = add("panel", undefined, panel.selectDirMenuTitle);
             with(myWindow.formPath){
                 myWindow.formPath.alignChildren = "left";
                 myWindow.formPath.inputPath = add("edittext");
@@ -133,38 +172,47 @@ function drawWindow(){
             }
             myWindow.qualityGroup = add("panel", undefined, panel.qualityTitle);
             with(myWindow.qualityGroup){
-                myWindow.qualityGroup.orientation = "column";
-                myWindow.qualityGroup.alignChildren = "left";
-                myWindow.qualityGroup.formQuality = add("group");
+                qualityGroup.orientation = "column";
+                qualityGroup.alignChildren = "left";
+                qualityGroup.formQuality = add("group");
                 with(myWindow.qualityGroup.formQuality){
-                    myWindow.qualityGroup.formQuality.orientation = "row";
+                    formQuality.orientation = "row";
                     for (i = 0; i < panel.qualityValues.length; i++) {
-                        myWindow.qualityGroup.formQuality.add ("radiobutton", undefined, (panel.qualityValues[i]));
+                        formQuality.add ("radiobutton", undefined, (panel.qualityValues[i]));
                     }
-                    myWindow.qualityGroup.formQuality.children[image.exportQuality].value =true;
+                    formQuality.children[image.exportQuality].value =true;
                 }
                 myWindow.qualityGroup.formDensity = add( "group");
                 with(myWindow.qualityGroup.formDensity){
-                    myWindow.qualityGroup.formDensity.add ("statictext", undefined, panel.densityTitle);
-                    myWindow.qualityGroup.formDensity.inputDPI = add ("edittext", undefined, image.exportDPI);
-                    myWindow.qualityGroup.formDensity.inputDPI.characters = 4;
-                    myWindow.qualityGroup.formDensity.sliderDPI = add("slider", undefined, image.exportDPI, image.minExportDPI, image.maxExportDPI);
+                    formDensity.inputDPI = add ("edittext", undefined, image.exportDPI);
+                    formDensity.inputDPI.characters = 4;
+                    formDensity.sliderDPI = add("slider", undefined, image.exportDPI, image.minExportDPI, image.maxExportDPI);
+                    formDensity.add ("statictext", undefined, panel.densityTitle);
                     // synchronize slider and input field
-                    myWindow.qualityGroup.formDensity.sliderDPI.onChanging = function () {myWindow.qualityGroup.formDensity.inputDPI.text = myWindow.qualityGroup.formDensity.sliderDPI.value};
-                    myWindow.qualityGroup.formDensity.inputDPI.onChanging = function () {myWindow.qualityGroup.formDensity.sliderDPI.value = myWindow.qualityGroup.formDensity.inputDPI.text};
+                    formDensity.sliderDPI.onChanging = function () {myWindow.qualityGroup.formDensity.inputDPI.text = myWindow.qualityGroup.formDensity.sliderDPI.value};
+                    formDensity.inputDPI.onChanging = function () {myWindow.qualityGroup.formDensity.sliderDPI.value = myWindow.qualityGroup.formDensity.inputDPI.text};
                 }
+            }
+            myWindow.optionsGroup = add("panel", undefined, panel.optionsTitle);
+            with(myWindow.optionsGroup){
+              myWindow.optionsGroup.maxResolutionGroup = add("group");
+              with(myWindow.optionsGroup.maxResolutionGroup){
+                maxResolutionGroup.inputMaxRes = maxResolutionGroup.add("edittext", undefined, image.maxResolution);
+                maxResolutionGroup.add("statictext", undefined, panel.maxResolutionTitle);
+              }
             }
             myWindow.buttonGroup = add( "group");
             with(myWindow.buttonGroup){
                 myWindow.buttonGroup.orientation = "row";
-                myWindow.buttonGroup.buttonOK = add ("button", undefined, "OK");
-                myWindow.buttonGroup.buttonCancel = add ("button", undefined, "Cancel");
+                myWindow.buttonGroup.buttonOK = add ("button", undefined, panel.buttonOK, {name: "ok"});
+                myWindow.buttonGroup.buttonCancel = add ("button", undefined, panel.buttonCancel, {name: "cancel"} );
             }
         }
     myWindow.formPath.buttonChoosePath.onClick  = function () {
         var result = Folder.selectDialog ();
         if (result) {
             myWindow.formPath.inputPath.text = result;
+
         }
     }
     myWindow.buttonGroup.buttonOK.onClick = function(){
@@ -172,12 +220,13 @@ function drawWindow(){
         image.exportDir = Folder(myWindow.formPath.inputPath.text);
         image.exportDPI = Number(myWindow.qualityGroup.formDensity.inputDPI.text);
         image.exportQuality = selectedRadiobutton(myWindow.qualityGroup.formQuality);
+        image.maxResolution = Number(myWindow.optionsGroup.maxResolutionGroup.inputMaxRes.text);
         myWindow.close(1);
 
         function selectedRadiobutton (rbuttons){
             for(var i = 0; i < rbuttons.children.length; i++) {
                 if(rbuttons.children[i].value == true) {
-                    return i;                    
+                    return i;
                 }
             }
         }
@@ -190,32 +239,46 @@ function drawWindow(){
 
 
 function getFilelinks(doc){
+
     var docLinks = doc.links;
     var uniqueBasenames = [];
-    var exportLinks = []    
+    var exportLinks = []
     /*
      * rename files if a basename exists twice
      */
     for (var i = 0; i < docLinks.length; i++) {
         var link = docLinks[i];
+        var originalBounds = link.parent.parent.geometricBounds;
+        var rectangle =  cropRectangleToBleeds(link.parent.parent);
+        var normalizedDensity = getMaxDensity(image.exportDPI, rectangle, image.maxResolution);
+
         if(isValidLink(link)){
             var basename = getBasename(link.name);
-            if(inArray(basename, uniqueBasenames)) {
-                var newFilename = renameFile(basename, image.exportFormat, true);
+            // use existing filename label
+            if(rectangle.extractLabel(image.pageItemLabel).length > 0){
+              var newFilename = rectangle.extractLabel(image.pageItemLabel);
+            // generate new filename if basename exists twice
+            }else if(inArray(basename, uniqueBasenames)) {
+                var newFilename = renameFile(basename, "jpg", true);
             } else {
                 uniqueBasenames.push(basename);
-                var newFilename = renameFile(basename, image.exportFormat, false);
+                var newFilename = renameFile(basename, "jpg", false);
             }
             /*
              * construct link object
              */
             linkObject = {
                 link:link,
-                pageItem:link.parent.parent,
+                pageItem:rectangle,
+                quality:image.exportQuality,
+                density:normalizedDensity,
                 newFilename:newFilename,
-                newFilepath:File(image.exportDir + "/" + newFilename)
+                newFilepath:File(image.exportDir + "/" + newFilename),
+                originalBounds:originalBounds
             }
-
+            /*
+             * check for custom object export options
+             */
             exportLinks.push(linkObject);
 
         } else {
@@ -227,44 +290,45 @@ function getFilelinks(doc){
         if (!result) return;
     }
     // create directory
-    createDir(image.exportDir);    
+    createDir(image.exportDir);
 
     if (exportLinks.length  > 0) {
-        with (app.jpegExportPreferences) {
-            antiAlias = false;
-            embedColorProfile = false;
-            exportResolution= image.exportDPI;
-            jpegColorSpace = JpegColorSpaceEnum.RGB;
-            jpegExportQualityValues = [JPEGOptionsQuality.MAXIMUM ,JPEGOptionsQuality.HIGH, JPEGOptionsQuality.MEDIUM, JPEGOptionsQuality.LOW];
-            jpegQuality = jpegExportQualityValues[image.exportQuality];
-            jpegRenderingStyle = JPEGOptionsFormat.BASELINE_ENCODING  
-            simulateOverprint = true; 
-            useDocumentBleeds = true;
-        }
-
         //var progressBar = getProgressBar(exportLinks.length);
         var progressBar = getProgressBar("export " + exportLinks.length + " images");
         progressBar.reset(exportLinks.length);
 
-        // select export format
-        var exportFormat = ExportFormat.JPG
-        
+
         // iterate over files and store to specific location
         for (i = 0; i  < exportLinks.length; i++) {
-            
-            progressBar.hit("export " + exportLinks[i].newFilename, i);
+          var exportFormat = ExportFormat.JPG;
+          var exportResolution = exportLinks[i].density;
+          var exportQuality = exportLinks[i].quality;
+          // JPEG export options
+          app.jpegExportPreferences.antiAlias = false;
+          app.jpegExportPreferences.embedColorProfile = false;
+          app.jpegExportPreferences.exportResolution = exportResolution;
+          app.jpegExportPreferences.jpegColorSpace = JpegColorSpaceEnum.RGB;
+          jpegExportQualityValues = [JPEGOptionsQuality.MAXIMUM ,JPEGOptionsQuality.HIGH, JPEGOptionsQuality.MEDIUM, JPEGOptionsQuality.LOW];
+          app.jpegExportPreferences.jpegQuality = jpegExportQualityValues[exportQuality];
+          app.jpegExportPreferences.jpegRenderingStyle = JPEGOptionsFormat.BASELINE_ENCODING;
+          app.jpegExportPreferences.simulateOverprint = true;
+          app.jpegExportPreferences.useDocumentBleeds = true;
 
-            exportLinks[i].pageItem.exportFile(exportFormat, exportLinks[i].newFilepath);
-            // insert label with new file link for postprocessing
-            exportLinks[i].pageItem.insertLabel(image.pageItemLabel, exportLinks[i].newFilename);
+          progressBar.hit("export " + exportLinks[i].newFilename, i);
+
+          exportLinks[i].pageItem.exportFile(exportFormat, exportLinks[i].newFilepath);
+          // insert label with new file link for postprocessing
+          exportLinks[i].pageItem.insertLabel(image.pageItemLabel, exportLinks[i].newFilename);
+          // restore original bounds
+          exportLinks[i].pageItem.geometricBounds = exportLinks[i].originalBounds;
         }
         progressBar.close();
 
-        alert (exportLinks.length  + " images exported.");
+        alert (exportLinks.length  + " " + panel.finishedMessage);
         doc.save();
     }
     else {
-        alert (panel.noValidLinks);     
+        alert (panel.noValidLinks);
     }
 }
 // draw simple progress bar
@@ -305,7 +369,7 @@ function isValidLink (link) {
         default:
             if(link != null) return true else return false;
     }
-} 
+}
 // return filename with new extension and conditionally attach random string
 function renameFile(basename, extension, rename) {
     if(rename) {
@@ -331,6 +395,41 @@ function inArray(string, array) {
     }
     return false;
 }
+
+// get density limit according to maximal resolution value
+function getMaxDensity(density, rectangle, maxResolution) {
+  var bounds = rectangle.geometricBounds;
+  var baseMultiplier = 72;
+  var densityFactor = density / baseMultiplier;
+  var width =  (bounds[3] - bounds[1]);
+  var height = (bounds[2] - bounds[0]);
+  var resolution = width * height * Math.pow(densityFactor, 2);
+  if(resolution > maxResolution) {
+    var maxDensity =  Math.floor(Math.sqrt(maxResolution * Math.pow(densityFactor, 2) / resolution) * baseMultiplier);
+    return maxDensity;
+  } else {
+    return density;
+  }
+}
+
+function cropRectangleToBleeds (rectangle){
+  bounds = rectangle.geometricBounds;
+  var page = rectangle.parentPage;
+  // iterate over corners and fit them into page
+  var newBounds = [];
+  for(var i = 0; i <= 3; i++) {
+    if((i == 0 || i == 1 ) && bounds[i] < page.bounds[i]){
+      newBounds[i] = page.bounds[i];
+    } else if((i == 2  || i == 3 ) && bounds[i] > page.bounds[i]){
+      newBounds[i] = page.bounds[i];
+    } else {
+      newBounds[i] = rectangle.geometricBounds[i];
+    }
+  }
+  rectangle.geometricBounds = newBounds;
+  return rectangle;
+}
+
 // get path relative to indesign file location
 function getDefaultExportPath() {
     var exportPath = String(app.activeDocument.fullName);
